8883e6f006e02fc3531e6f4b61ccd66abdf288e8
veselat123 <veselatveselat@gmail.com>
2023-09-06 19:19:39 +0600

    home2

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,34 @@
+import java.util.Random;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(mojnoIdtiGulyati(11,15));
-        System.out.println(mojnoIdtiGulyati(11,15));
-        System.out.println(mojnoIdtiGulyati(11,15));
-        System.out.println(mojnoIdtiGulyati(11,15));
-        System.out.println(mojnoIdtiGulyati(11,15));
-
-
-        System.out.println("hello word");
+        System.out.println(mojnoIdtiGulyati(11, 15));
+        System.out.println(mojnoIdtiGulyati(0, 80));
+        System.out.println(mojnoIdtiGulyati(15, 26));
+        System.out.println(mojnoIdtiGulyati(12, 56));
+        System.out.println(mojnoIdtiGulyati(generateRandomAge(),33));
     }
 
     public static String mojnoIdtiGulyati(int age, int temperature) {
         if (age > 20 && age < 45 && temperature > -20 && temperature < 30) {
             return "Можно идти гулять";
-        } else if (age < 20 && temperature > -20 && temperature < 30) {
+        } else if (age < 20 && temperature > 0 && temperature < 28) {
             return "можно идти гулять ";
         } else if (age > 45 && temperature > -10 && temperature < 25) {
             return "Можно идти гулять";
         } else {
-            return"Оставайтесь дома ";
+            return "Оставайтесь дома ";
+
         }
     }
 
-
+    public static int generateRandomAge() {
+        Random randomAge = new Random();
+        return randomAge.nextInt(100);
+    }
 }
+
+
+
